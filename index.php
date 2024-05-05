75d47dde9f811b7e1cebb8aa974c2bd0c3dd31dc
dominique-789 <155193534+dominique-789@users.noreply.github.com>
2024-05-05 08:25:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr" data-bs-theme="dark">
+
+<head>
+    <title>AMSG</title>
+    <link rel="icon" href="image/arts-martiaux.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+    <!--<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kotta+One&family=Quintessential&display=swap" rel="stylesheet">-->
+
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="./CSS/style.css" />
+</head>
+
+<body>
+
+    <header>
+        <!--.......................................Logo et navbar.....................................................-->
+        <main>
+            <div class="bloc">
+                <nav class="navbar navbar-expand-sm bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="index.php"></a>AMSG</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapsibleNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php">Accueil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="history.php">Histoire</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="child.php">Enfants</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="actu.php">Actus</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown">Disciplines</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="self.php">Self Defense</a></li>
+                                        <li><a class="dropdown-item" href="ken.php">Kendo</a></li>
+                                        <li><a class="dropdown-item" href="iai.php">Iaido</a></li>
+                                        <li><a class="dropdown-item" href="aik.php">Aikido</a></li>
+                                        <li><a class="dropdown-item" href="ku.php">Kung Fu</a></li>
+                                        <li><a class="dropdown-item" href="sys.php">Systema</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="broc.php">Brochure <i class="bi bi-download"></i></a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="santeadul.php">Santé adulte <i
+                                            class="bi bi-lungs"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="inscri.php">Inscription <i class="bi bi-people"></i></a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <video autoplay="autoplay" muted="" loop="infinite"
+                    src="./video/istockphoto-1212586925-640_adpp_is.mp4"></video>
+
+
+                <div class="titre">
+                    <div>
+                        <h1>AMSG</h1>
+                        <br>
+                        <h4>Arts martiaux de Saint-Germain-en-Laye</h4>
+                    </div>
+                </div>
+            </div>
+            </main>
+    </header>
+
+
+</body>
+
+</html>
